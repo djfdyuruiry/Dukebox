e278689768403b1e0991d30723c4da1536579c39
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2013-10-08 18:44:54 +0100

    Renamed projects and cleaned directories, add build output to ignored files.

---
+++ Dukebox.Library/Model/LibraryToString.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dukebox.Library
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class song
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (artistId == null)
+            {
+                return "Unknown Artist" + " - " + title == string.Empty ? "Unknown" : title;
+            }
+            else if (artistId == -1)
+            {
+                return title == string.Empty ? "Unknown" : title;
+            }
+
+            return MusicLibrary.GetInstance().Artists.Where(a => a.id == artistId).FirstOrDefault() + " - " + title;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class artist
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class album
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
---
+++ Dukebox.Library/Model/Track.cs
@@ -0,0 +1,35 @@
+﻿using Dukebox.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dukebox.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Track
+    {
+        public song Song { get; set; }
+        public artist Artist { get; set; }
+        public album Album { get; set; }
+
+        public AudioFileMetaData Metadata { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (Song.artistId != null && Song.artistId == -1)
+            {
+                return (Artist.name == string.Empty ? "Unknown Artist" : Artist.name) + " - " + Song.ToString();
+            }
+
+            return Song.ToString();
+        }
+    }
+}
