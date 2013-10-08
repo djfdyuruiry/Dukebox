40be30d16ee8a66effe7ee2f72625141fe1e3c66
djfdyuruiry <djfdyuruiry@localhost>
2013-10-08 18:44:54 +0100

    Renamed projects and cleaned directories, add build output to ignored files.

---
+++ Dukebox.Audio/Formats.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dukebox.Audio
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class AudioFileFormats
+    {
+        /// <summary>
+        /// A string list of all supported audio
+        /// formats specified by file extension. (e.g. '.mp3')
+        /// </summary>
+        public static List<string> SupportedFormats = new List<string>();
+
+        /// <summary>
+        /// A file filter string for use with the file dialog
+        /// class which represents all supported formats.
+        /// </summary>
+        public static string FileDialogFilter
+        {
+            get
+            {
+                string formatList = SupportedFormats.Aggregate((a, b) => a + b).Replace(".", ";*.");
+                string filter = "Audio Files |" + formatList.Substring(1);
+
+                return filter;
+            }
+        }
+    } 
+}
---
+++ Dukebox.Library/Model/CdMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dukebox.Library.Model
+{
+    public class CdMetadata
+    {
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public List<string> Tracks { get; set; }
+    }
+}
