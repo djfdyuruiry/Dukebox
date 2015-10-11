c8d62a6a65574d729547b7689064bd1077667494
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2015-10-12 00:22:40 +0100

    Moving Dukebox.Library.Model.AudioFileImportedEventArgs to seperate file

---
+++ Dukebox.Library/Model/AudioFileImportedEventArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dukebox.Library.Model
+{
+    /// <summary>
+    /// Arguments in the event of an audio file being imported
+    /// or being pre-processed.
+    /// </summary>
+    public class AudioFileImportedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Is the track going through pre-processing?
+        /// </summary>
+        public bool JustProcessing { get; set; }
+
+        /// <summary>
+        /// The absolute filename that was added to the library.
+        /// </summary>
+        public string FileAdded { get; set; }
+
+        /// <summary>
+        /// Total files that are scheduled for this events parent operation.
+        /// </summary>
+        public int TotalFilesThisImport { get; set; }
+    }
+}
