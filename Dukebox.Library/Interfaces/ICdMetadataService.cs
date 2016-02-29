189549c9aa4628bc4be324efae89811d0cb361ce
djfdyuruiry <djfdyuruiry@localhost>
2016-02-29 21:54:13 +0000

    Port Desktop App to WPF: Refactor Library and Audio projects to use IOC packages

---
+++ Dukebox.Library/Interfaces/ICdMetadataService.cs
@@ -0,0 +1,14 @@
+﻿using Dukebox.Library.Model;
+using Dukebox.Model.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Dukebox.Library.Interfaces
+{
+    public interface ICdMetadataService
+    {
+        void CheckForCddbServerConnection();
+        List<AudioFileMetaData> GetAudioFileMetaDataForCd(char driveLetter);
+        CdMetadata GetMetadataForCd(char driveLetter);
+    }
+}
