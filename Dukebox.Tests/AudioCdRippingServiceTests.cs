5d9698946db9175ba4818ef06fe0c0a3dfa884d0
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2016-03-27 04:05:04 +0100

    Unit Tests: Audio CD Ripping Service

--- Dukebox.Tests/AudioCdRippingServiceTests.cs
+++ Dukebox.Tests/AudioCdRippingServiceTests.cs
@@ -1,28 +1,55 @@
 ﻿using Dukebox.Audio.Interfaces;
+using Dukebox.Audio.Services;
 using Dukebox.Library.Interfaces;
+using Dukebox.Library.Services;
 using Dukebox.Library.Services;
 using FakeItEasy;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Dukebox.Tests
 {
     public class AudioCdRippingServiceTests
     {
-        [Fact]
-        public void RipAudioCd()
+        private readonly string _audioDir;
+
+        public AudioCdRippingServiceTests()
         {
-            var musicLibrary = A.Fake<IMusicLibrary>();
-            var metadataService = A.Fake<ICdMetadataService>();
-            var audioConverterService = A.Fake<IAudioConverterService>();
+            _audioDir = string.Format(@".\{0}", "audioRipOut");
 
-            var audioRipper = new AudioCdRippingService(musicLibrary, metadataService, audioConverterService);
+            BassLoader.LoadBass();
 
-            // TODO: mock calls to dependencies
+            try
+            {
+                Directory.Delete(_audioDir, true);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+            Directory.CreateDirectory(_audioDir);
+        }
+
+        [Fact]
+        public async void RipAudioCd()
+        {
+            var metadataService = new CdMetadataService(new AudioCdService());
+            var audioConverterService = new AudioConverterService();
+
+            var audioRipper = new AudioCdRippingService(metadataService, audioConverterService);
+            var viewUpdater = A.Fake<ICdRipViewUpdater>();
+
+            await audioRipper.RipCdToFolder(AudioCdTestConstants.CdDrivePath, _audioDir, viewUpdater);
+
+            var audioCdTrackCount = Directory.EnumerateFiles(AudioCdTestConstants.CdDrivePath).Count();
+            var outputTrackCount = Directory.EnumerateFiles(_audioDir).Count();
+            var allTracksPresentInOutput = audioCdTrackCount == outputTrackCount;
+
+            Assert.True(allTracksPresentInOutput, string.Format("Failed to rip all Tracks from the audio CD ({0})", AudioCdTestConstants.CheckDriveMessage));
         }
     }
 }
