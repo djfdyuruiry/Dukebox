77bf5259ca9908cdfc6138e8e2877b36f36b662a
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2016-05-07 21:43:23 +0100

    Issue Fix: Add Playlist Files Fails

--- Dukebox.Library/Interfaces/IAudioFileMetaData.cs
+++ Dukebox.Library/Interfaces/IAudioFileMetaData.cs
@@ -9,11 +9,13 @@ namespace Dukebox.Library.Interfaces
 {
     public interface IAudioFileMetadata
     {
+        string FileName { get; }
         string Album { get; set; }
         Image AlbumArt { get; }
         string Artist { get; set; }
         bool HasAlbumArt { get; }
         bool HasFutherMetadataTag { get; }
+        bool IsEmpty { get; }
         int Length { get; }
         string Title { get; set; }
         void CommitChangesToFile();
