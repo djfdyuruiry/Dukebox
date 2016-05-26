ea633649af298279d916f0a259d0eb2f3e5fc1b1
djfdyuruiry <djfdyuruiry@users.noreply.github.com>
2016-05-27 00:35:58 +0100

    Create mechanism for Viewing tracks in Artist/Album without playing

---
+++ Dukebox.Desktop/Model/PreviewArtistOrAlbumMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Dukebox.Desktop.Model
+{
+    public class PreviewArtistOrAlbumMessage
+    {
+        public long Id { get; set; }
+        public bool IsArtist { get; set; }
+        public bool IsAlbum { get; set; }
+    }
+}
