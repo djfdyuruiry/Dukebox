69787278a1d806a8162d7f4c271bfa3417fe176b
djfdyuruiry <djfdyuruiry@hotmail.com>
2016-06-05 18:57:24 +0100

    Removing Music Library add entity tests

--- Dukebox.Tests/Unit/MusicLibraryTests.cs
+++ Dukebox.Tests/Unit/MusicLibraryTests.cs
@@ -37,7 +37,7 @@ namespace Dukebox.Tests.Unit
             _musicLibrary = new MusicLibrary(_musicLibraryDbContext, settings, albumArtCache, audioFormats);
 
         }
-        /*
+        
         [Fact]
         public void Album()
         {
@@ -241,7 +241,7 @@ namespace Dukebox.Tests.Unit
             Assert.True(resultsForKnownSong.Any(), "Failed to get tracks by known song id");
             Assert.False(resultsForMissingSong.Any(), "Incorrectly got results for an unknown song id");
         }
-
+        /*
         [Fact]
         public void AddDirectory()
         {
@@ -333,7 +333,7 @@ namespace Dukebox.Tests.Unit
 
             Assert.True(allFilesInPlaylist, "Music library failed to store all files for playlist");
         }
-
+        */
         [Fact]
         public async void RemoveTrack()
         {
@@ -362,6 +362,6 @@ namespace Dukebox.Tests.Unit
             }
 
             return files;
-        }*/
+        }
     }
 }
