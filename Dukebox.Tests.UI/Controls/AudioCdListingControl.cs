7a575dc23bf06cf714b2ea2c9a309f4c01c9b76b
djfdyuruiry <djfdyuruiry@hotmail.com>
2016-07-16 21:04:54 +0100

    Removed Disabled Search Control from AudioCdListingControl

--- Dukebox.Tests.UI/Controls/AudioCdListingControl.cs
+++ Dukebox.Tests.UI/Controls/AudioCdListingControl.cs
@@ -1,19 +1,15 @@
 ﻿using System.Windows.Automation;
 using TestStack.White.UIItems.Actions;
 using TestStack.White.UIItems.Custom;
-using TestStack.White.UIItems.WPFUIItems;
 
 namespace Dukebox.Tests.UI.Controls
 {
     [ControlTypeMapping(CustomUIItemType.Custom)]
     public class AudioCdListingControl : CustomUIItem
     {
-        public SearchControl SearchControl { get; private set; }
-
         public AudioCdListingControl(AutomationElement automationElement, ActionListener actionListener) 
             : base(automationElement, actionListener)
         {
-            SearchControl = this.Get<SearchControl>("SearchUserControl");
         }
 
         public AudioCdListingControl()
