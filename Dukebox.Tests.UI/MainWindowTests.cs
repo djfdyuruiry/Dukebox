fbe20c2d0ec25f937a4f191445fb0dbc64986391
djfdyuruiry <djfdyuruiry@hotmail.com>
2016-07-16 02:22:48 +0100

    Added some Screen Object and Inital Simple Test using Automation IDs

---
+++ Dukebox.Tests.UI/MainWindowTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.ScreenObjects;
+using Xunit;
+using Dukebox.Tests.UI.Screens;
+
+namespace Dukebox.Tests.UI
+{
+    public class MainWindowTests
+    {
+        private readonly Application _application;
+        private readonly ScreenRepository _screenRepository;
+
+        public MainWindowTests()
+        {
+            _application = Application.Launch(@"..\..\..\..\Dukebox.Desktop\bin\x64\Debug\Dukebox.exe");
+            _screenRepository = new ScreenRepository(_application.ApplicationSession);
+
+            var initalImportScreen = _screenRepository.Get<InitalImportScreen>("Dukebox - Welcome", InitializeOption.NoCache);
+            initalImportScreen.SkipImport();
+        }
+
+        [Fact]
+        public void When_File_Menu_Exit_Item_Clicked_App_Should_Close()
+        {
+            var mainScreen = _screenRepository.Get<MainScreen>("Dukebox", InitializeOption.NoCache);
+            mainScreen.Exit();
+
+            Thread.Sleep(250);
+
+            var appIsClosed = _application.ApplicationSession.Application.HasExited;
+
+            Assert.True(appIsClosed);
+        }
+    }
+}
---
+++ Dukebox.Tests.UI/Screens/MainScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using TestStack.White.ScreenObjects;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.WindowStripControls;
+
+namespace Dukebox.Tests.UI.Screens
+{
+    public class MainScreen : AppScreen
+    {
+        protected MenuBar ToolbarMenu;
+
+        public MainScreen(Window window, ScreenRepository screenRepository) : base(window, screenRepository)
+        {
+        }
+
+        public virtual void Exit()
+        {
+            var fileMenu = ToolbarMenu.MenuItemBy(SearchCriteria.ByAutomationId("FileMenu"));
+
+            fileMenu
+                .ChildMenus
+                .First(m => m.AutomationElement.Current.AutomationId == "ExitMenuItem")
+                .Click();
+        }
+    }
+}
